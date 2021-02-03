a89bf734ab8782213a169fe054825d5ab5dcd576
iskanderskr <iskander.bittencourt42@gmail.com>
2021-02-03 17:56:52 -0300

    send

--- index.js
+++ index.js
@@ -5,16 +5,15 @@ const wss = new WebSocket.Server({ port: PORT })
 
 wss.on('connection', ws => {
     console.log('Um conexão iniciada')
-})
-
-wss.on('message', message => {
-    const obj = JSON.parse(message)
-    console.log(message)
-    if(obj.type ==  'ura'){
-        ws.send(JSON.stringify({
-            type: 'sf',
-            name: obj.name,
-            cpf: obj.cpf
-        }))
-    }
+    wss.on('message', message => {
+        const obj = JSON.parse(message)
+        console.log(message)
+        if(obj.type ==  'ura'){
+            wss.broadcast(JSON.stringify({
+                type: 'sf',
+                name: obj.name,
+                cpf: obj.cpf
+            }));
+        }
+    })
 })
