97f05a637541cbe36d2452e0485b8fc0f0d1c35a
iskanderskr <iskander.bittencourt42@gmail.com>
2021-02-03 18:00:54 -0300

    send

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const WebSocket = require('ws')
 const PORT = process.env.PORT || 5000
 const wss = new WebSocket.Server({ port: PORT })
 
+wss.broadcast = function broadcast(msg) {
+    wss.clients.forEach(function each(client) {
+        client.send(msg);
+     });
+ };
+
 wss.on('connection', ws => {
     console.log('Um conexão iniciada')
     ws.on('message', message => {
