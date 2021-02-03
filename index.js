908045be115fd8df75e7629a07b957ed196c95ce
iskanderskr <iskander.bittencourt42@gmail.com>
2021-02-03 17:54:27 -0300

    send

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
 const WebSocket = require('ws')
-const app = require('express')()
 
 const PORT = process.env.PORT || 5000
 const wss = new WebSocket.Server({ port: PORT })
 
 wss.on('connection', ws => {
     console.log('Um conexão iniciada')
+})
 
-    ws.on('message', message => {
-        const obj = JSON.parse(message)
-        console.log(message)
-        if(obj.type ==  'ura'){
-            ws.send(JSON.stringify({
-                type: 'sf',
-                name: obj.name,
-                cpf: obj.cpf
-            }))
-        }
-    })
+wss.on('message', message => {
+    const obj = JSON.parse(message)
+    console.log(message)
+    if(obj.type ==  'ura'){
+        ws.send(JSON.stringify({
+            type: 'sf',
+            name: obj.name,
+            cpf: obj.cpf
+        }))
+    }
 })
